8fbf1225063d33915945d551693d857310af563a
webdevnel <hananel.delcoro@gmail.com>
2025-07-25 13:57:30 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+//How to run in the CMD 
+// Microsoft Windows [Version 10.0.26100.4652]
+// (c) Microsoft Corporation. All rights reserved.
+
+// C:\Users\Nel>cd desktop //where your notepad file is
+
+// C:\Users\Nel\Desktop>javac HelloWorld.java
+
+// C:\Users\NelDesktop>java HelloWorld
+// Hello, world!
+
+// C:\Users\Nel\Desktop>exit
